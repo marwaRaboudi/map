96ed2696446f5c189c729f41f0ef2cdb6562cd2a
marwa raboudi <mraboudi@intimeco.net>
2019-03-27 15:41:02 +0100

    add setMap function

--- src/map.js
+++ src/map.js
@@ -1,11 +1,15 @@
-class Map {
-  constructor(mapDiv, mapOptions) {
-    this.mapDiv = mapDiv;
-    this.mapOptions = mapOptions;
-  }
+let google;
 
-  setMap() {
-    return "map is set and ready";
+class Map {
+
+  setMap(DivId, centerLat, centerLng, zoom) {
+    let mapDiv = document.getElementById(DivId);
+    let map = new google.maps.Map(mapDiv, {
+      center: {lat: centerLat, lng: centerLng},
+      zoom: zoom
+    });
+    console.log("map is set and ready");
+    return map;
   }
 
 }
